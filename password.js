577cdbdbe96b16ced9f3c8ca164875440a6bd70a
Laura Bizzle <105003600+laura-bizzle@users.noreply.github.com>
2023-11-09 10:40:13 +0000

    password cookie added

--- password.js
+++ password.js
@@ -1,5 +1,31 @@
 const correctPassword = "open"; // Set your password here
 
+// window.onload = function() {
+    
+// }
+
+function logoAnimations() {
+
+    // Add animations
+    document.querySelectorAll('.letters').forEach((letter, index) => {
+       letter.style.animation = `upAndDown 2s ${index * 0.1}s`;
+   });
+   
+      document.querySelector('.x').style.animation = 'spinny 0.4s 2.2s 1 forwards';
+      document.querySelector('.pata-logo').style.animation = 'grow 0.4s 2.5s 1 forwards';
+      document.querySelector('.down-arrow').style.animation = 'jump 1.5s 3s ease-in-out 3';
+};
+
+if (document.cookie.includes("passwordSeen=true")) {
+    // If the cookie is set, don't show the password prompt
+    document.body.style.overflow = 'auto';
+    hidePassword();
+    logoAnimations();
+  } else {
+    document.body.style.overflow = 'hidden';
+  }
+  
+
 document.querySelector('.password-btn').addEventListener('click' , checkPassword);
 document.querySelector('#password-input').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
@@ -14,7 +40,15 @@ function checkPassword() {
     const content = document.querySelector('.content');
     const overlayContent = document.querySelector('.overlay-content')
 
+    
+
     if (input.value === correctPassword) {
+        // Set a cookie to remember that the user has seen the password
+        document.cookie = "passwordSeen=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+
+         hidePassword();
+         logoAnimations();
+        
         document.body.style.overflow = 'auto';
         overlayContent.classList.add('hide');
         setTimeout(function () {
@@ -23,25 +57,18 @@ function checkPassword() {
           setTimeout(function () {
             overlay.style.display = 'none';
           }, 700);
-        // 
         // content.style.display = 'block';
-        
-        // Add animations
-        document.querySelectorAll('.letters').forEach((letter, index) => {
-            letter.style.animation = `upAndDown 2s ${index * 0.1}s`;
-        });
-        
-    // document.querySelector('.op-logo').style.animation = 'someAnimation 1s';  // Define someAnimation in your CSS
-        
-        document.querySelector('.x').style.animation = 'spinny 0.4s 2.2s 1 forwards';
-        
-        document.querySelector('.pata-logo').style.animation = 'grow 0.4s 2.5s 1 forwards';
-        
-        document.querySelector('.down-arrow').style.animation = 'jump 1.5s 3s ease-in-out 3';
         
     } else {
         alert('Incorrect Password!');
     }
+}
+
+function hidePassword() {
+    const overlay = document.getElementById('overlay');
+    const overlayContent = document.querySelector('.overlay-content');
+    overlay.style.display = 'none';
+    
 }
 
 
@@ -49,6 +76,3 @@ document.querySelector('.overlay').style.backgroundImage = `url(${client.heroImg
 document.querySelector('.overlay').style.background = `linear-gradient(0deg, rgba(0, 0, 0, 0.50) 0%, rgba(0, 0, 0, 0.50) 100%), url(<path-to-image>), lightgray 50% / cover no-repeat;`
 
 
-window.onload = function() {
-    document.body.style.overflow = 'hidden';
-}
